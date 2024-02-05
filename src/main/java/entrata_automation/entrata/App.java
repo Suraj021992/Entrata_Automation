032529eac1662c9dc7f81237814b7e0afa72f49f
Suraj021992 <shelakesuraj02@gmail.com>
2024-02-05 20:17:20 +0530

    maven project created

---
+++ src/main/java/entrata_automation/entrata/App.java
@@ -0,0 +1,13 @@
+package entrata_automation.entrata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
